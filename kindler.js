cd731de8c5f3efffae8a3ada7bc43836a3d9ebf4
skn <660462+skn@users.noreply.github.com>
2021-05-24 13:54:03 +0400

    Update kindler.js
    js change to send URL as GET. Popup used.

--- kindler.js
+++ kindler.js
@@ -1,29 +1,3 @@
-javascript: (function() {
-    var html = document.getElementsByClassName("entry-content")[0].innerHTML;
-    var form = document.createElement("form");
-    document.body.appendChild(form);
-    form.action = "http://127.0.0.1:8081";
-    form.method = "POST";
-
-    var textarea = document.createElement("textarea");
-    textarea.name = "contents";
-    textarea.value = html;
-
-    var title = document.createElement("input");
-    title.name = "title";
-    title.value = document.getElementsByClassName("entry-title")[0].innerHTML;
-    title.value = prompt('Please check the title', title.value); 
-
-    if (title.value != '' && title.value != null) {
-        form.appendChild(textarea);
-        form.appendChild(title);
-        form.submit();
-    }
-    function removeElement(id) {
-        try{
-            var element = document.getElementById(id);
-            element.parentNode.removeChild(element);
-        }
-        catch(e) {}
-    };
-})();
+javascript: q = location.href;
+p = document.title;
+void(open('https://host.com:port/?url=' + encodeURIComponent(q), 'Kindler', 'toolbar=no,width=500,height=150'));
